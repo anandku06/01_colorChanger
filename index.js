4b3d88e56fe6b68af338dd56726f92506ba3ef81
Commiter <161126335+anandku06@users.noreply.github.com>
2024-11-28 23:17:40 +0530

    add: implement color scheme switcher functionality in index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const btns = document.querySelectorAll(".button")
+const body = document.querySelector("body")
+
+btns.forEach((btn) => {
+    btn.addEventListener('click', (e)=> {
+        // console.log(e)
+        // console.log(e.target) // returns which element invoked the event
+        switch(e.target.id){
+            case 'grey':
+                body.style.backgroundColor = e.target.id
+                break;
+            case 'white':
+                body.style.backgroundColor = e.target.id
+                break;
+            case 'blue':
+                body.style.backgroundColor = e.target.id
+                break;
+            case 'yellow':
+                body.style.backgroundColor = e.target.id
+                break;
+            default:
+                body.style.backgroundColor = "black"            
+        }
+    }) // Appends an event listener for events whose type attribute value is type. The callback argument sets the callback that will be invoked when the event is dispatched.
+})
